8e71e714d6a17bfcad962a6183e5d2df9b8bc39b
Safx <safxdev@gmail.com>
2016-09-18 00:45:44 +0900

    swift-3

--- afx/afx_cmd.swift
+++ afx/afx_cmd.swift
@@ -1,39 +1,41 @@
-#!/usr/bin/swift
+//#!/usr/bin/swift
 
 import AppKit
 
 let token = "80F81AC50DA4458D8C46AFA3DCF8E451"
 
-let pb = NSPasteboard.generalPasteboard()
-let home = String.fromCString(getenv("HOME")) ?? ""
+let pb = NSPasteboard.general()
+let home = String(validatingUTF8: getenv("HOME")) ?? ""
 let cmd_prefix = token + " "
 
 while true {
     sleep(1)
-    guard let s = pb.stringForType(NSPasteboardTypeString) where s.hasPrefix(cmd_prefix) else {
+    guard let s = pb.string(forType: NSPasteboardTypeString), s.hasPrefix(cmd_prefix) else {
         continue
     }
 
     pb.clearContents()
 
-    let start = s.startIndex.advancedBy(cmd_prefix.characters.count)
-    let end = s.endIndex
-    let cmd = s.substringWithRange(start..<end)
+    let start = s.index(after: cmd_prefix.endIndex)
+    let cmd = s.substring(from: start)
 
+    print(cmd)
     let bs = "__(BACKSLASH)__"
     let cmdx = cmd
-               .stringByReplacingOccurrencesOfString("\\\\", withString: bs)
-               .stringByReplacingOccurrencesOfString("C:\\", withString: "\\")
-               .stringByReplacingOccurrencesOfString("Z:\\", withString: "\\")
-               .stringByReplacingOccurrencesOfString("\\", withString: "/")
-               .stringByReplacingOccurrencesOfString("$HOME", withString: home)
-               .stringByReplacingOccurrencesOfString(bs, withString: "\\")
+        .replacingOccurrences(of: "\\\\"    , with: bs)
+        .replacingOccurrences(of: "C:\\"    , with: "\\")
+        .replacingOccurrences(of: "Z:\\"    , with: "\\")
+        .replacingOccurrences(of: "\\"      , with: "/")
+        .replacingOccurrences(of: "$HOME"   , with: home)
+        .replacingOccurrences(of: bs        , with: "\\")
 
     print(cmdx)
-    cmdx.withCString { c -> () in
-        let ret = system(c)
-        if ret != 0 {
-            print("Error")
-        }
+
+    var args = cmdx.components(separatedBy: "\t")
+    guard let path = args.first else {
+        continue
     }
+    args.remove(at: 0)
+    print("->", path, args)
+    Process.launchedProcess(launchPath: path, arguments: args)
 }
